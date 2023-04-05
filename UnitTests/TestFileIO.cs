3c8021e842c9e79521fa4fa8ff9ab9d88dc1aac7
vijayakumar bhaskar <vijayakumar.kup@gmail.com>
2023-04-05 22:08:50 +0100

    Unit Tests

---
+++ UnitTests/TestFileIO.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using SafeTingz;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class TestFileIo
+    {
+        [Test]
+        public void Test1()
+        {
+            FileIO fileIo = new FileIO();
+            fileIo._filePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)+"/../../TestDocuments/MiniTestFile.txt";
+            fileIo.ReadFile();
+            Assert.NotNull(fileIo.Data);
+        }
+    }
+}
